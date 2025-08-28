4bb27cc0dfe7d78b864828f6ca3bdb4bcab14483
aleksi-svaridze <aleksi.svaridze82@gmail.com>
2025-08-28 11:58:34 +0400

    Finish css styling

--- src/app/card/card.ts
+++ src/app/card/card.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
 
+interface ICard {
+  card__image: string;
+
+  card__title: string;
+  card__description: string;
+
+  card__info_stats: number;
+  card__info_stats__icon: string;
+
+  card__info_deal: number;
+  card__info_deal__icon: string;
+
+  card__author_name: string;
+  card__author_image: string;
+}
+
 @Component({
   selector: 'app-card',
   imports: [],
   templateUrl: './card.html',
-  styleUrl: './card.scss'
+  styleUrl: './card.scss',
 })
 export class Card {
+  card: ICard = {
+    card__image: 'assets/card-img.png',
 
+    card__title: 'equilibrium #3429',
+    card__description: ' Our <span>equilibrium</span> collection promotes balance and calm.',
+
+    card__info_stats: 0.041,
+    card__info_stats__icon: 'assets/stats-icon.png',
+
+    card__info_deal: 3,
+    card__info_deal__icon: 'assets/clock-icon.png',
+
+    card__author_name: 'Jules Wyvern',
+    card__author_image: 'assets/Oval.png',
+  };
 }
